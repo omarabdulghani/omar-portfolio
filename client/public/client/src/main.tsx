ebfbed8908824e77476e791b43ca9711edfbdab4
omarabdulghani <omarabdulgh@gmail.com>
2026-03-13 01:36:14 +0100

    Configure Umami analytics

--- client/public/client/src/main.tsx
+++ client/public/client/src/main.tsx
@@ -2,4 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const analyticsScriptUrl = import.meta.env.VITE_ANALYTICS_SCRIPT_URL;
+const analyticsWebsiteId = import.meta.env.VITE_ANALYTICS_WEBSITE_ID;
+
+if (analyticsScriptUrl && analyticsWebsiteId && !document.querySelector('script[data-website-id]')) {
+  const script = document.createElement("script");
+  script.defer = true;
+  script.src = analyticsScriptUrl;
+  script.setAttribute("data-website-id", analyticsWebsiteId);
+  script.setAttribute("data-do-not-track", "true");
+  document.head.appendChild(script);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
